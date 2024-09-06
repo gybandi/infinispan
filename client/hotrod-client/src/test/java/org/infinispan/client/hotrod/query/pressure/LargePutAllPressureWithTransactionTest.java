bfbba5469d8025937586d020b33e2693a558bc6c
Gyuró András <andras.gyuro@navitasoft.com>
2024-09-06 17:20:05 +0200

    ISPN-16514 Changed LargePutAllPressureWithTransactionTest to more reasonable use case

--- client/hotrod-client/src/test/java/org/infinispan/client/hotrod/query/pressure/LargePutAllPressureWithTransactionTest.java
+++ client/hotrod-client/src/test/java/org/infinispan/client/hotrod/query/pressure/LargePutAllPressureWithTransactionTest.java
@@ -3,10 +3,10 @@ package org.infinispan.client.hotrod.query.pressure;
 import static org.infinispan.client.hotrod.impl.Util.await;
 import static org.infinispan.configuration.cache.IndexStorage.LOCAL_HEAP;
 import static org.infinispan.query.aggregation.QueryAggregationCountTest.CHUNK_SIZE;
-import static org.infinispan.query.aggregation.QueryAggregationCountTest.chunk;
 
 import java.util.HashMap;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import org.infinispan.client.hotrod.RemoteCache;
@@ -15,6 +15,7 @@ import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.IsolationLevel;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.protostream.SerializationContextInitializer;
+import org.infinispan.query.aggregation.QueryAggregationCountTest;
 import org.infinispan.query.model.Sale;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.infinispan.transaction.LockingMode;
@@ -35,22 +36,22 @@ public class LargePutAllPressureWithTransactionTest extends SingleHotRodServerTe
       ConfigurationBuilder config = new ConfigurationBuilder();
       config.indexing().enable()
             .storage(LOCAL_HEAP)
-               .addIndexedEntity("Sale")
+            .addIndexedEntity("Sale")
             .writer()
-               .queueCount(1)
-               .queueSize(10_000)
+            .queueCount(1)
+            .queueSize(10_000)
             .locking().isolationLevel(IsolationLevel.REPEATABLE_READ)
             .transaction()
-               .transactionManagerLookup(new EmbeddedTransactionManagerLookup())
-               .transactionMode(TransactionMode.TRANSACTIONAL)
-               .lockingMode(LockingMode.PESSIMISTIC);
+            .transactionManagerLookup(new EmbeddedTransactionManagerLookup())
+            .transactionMode(TransactionMode.TRANSACTIONAL)
+            .lockingMode(LockingMode.PESSIMISTIC);
 
       return TestCacheManagerFactory.createServerModeCacheManager(config);
    }
 
    @Override
    protected org.infinispan.client.hotrod.configuration.ConfigurationBuilder
-      createHotRodClientConfigurationBuilder(String host, int serverPort) {
+   createHotRodClientConfigurationBuilder(String host, int serverPort) {
       org.infinispan.client.hotrod.configuration.ConfigurationBuilder builder =
             super.createHotRodClientConfigurationBuilder(host, serverPort);
 
@@ -68,14 +69,27 @@ public class LargePutAllPressureWithTransactionTest extends SingleHotRodServerTe
    public void test() {
       RemoteCache<Object, Object> remoteCache = remoteCacheManager.getCache();
       int days = SIZE / CHUNK_SIZE;
-      HashMap<String, Sale> bulkPut = new HashMap<>(SIZE);
-      for (int day = 1; day <= days; day++) {
-         bulkPut.putAll(chunk(day, fixedSeedPseudoRandom));
+      for (int batch = 0; batch < NUMBER_OF_BATCHES; batch++) {
+         HashMap<String, Sale> bulkPut = new HashMap<>(SIZE);
+         for (int day = 1; day <= days; day++) {
+            bulkPut.putAll(chunkWithUuidKey(day, fixedSeedPseudoRandom));
+         }
+
+         CompletableFuture<Void> voidCompletableFuture = remoteCache.putAllAsync(bulkPut);
+         await(voidCompletableFuture);
       }
 
-      for(int batch = 0; batch < NUMBER_OF_BATCHES; batch++){
-         CompletableFuture<Void> voidCompletableFuture = remoteCache.putAllAsync(bulkPut);
-         await( voidCompletableFuture );
+   }
+
+   public static HashMap<String, Sale> chunkWithUuidKey(int day, Random random) {
+      HashMap<String, Sale> bulk = new HashMap<>();
+      for (int ordinal = 0; ordinal < CHUNK_SIZE; ordinal++) {
+         String id = UUID.randomUUID().toString();
+         QueryAggregationCountTest.Status status = QueryAggregationCountTest.Status.values()[random.nextInt(QueryAggregationCountTest.Status.values().length)];
+         String code = (ordinal % QueryAggregationCountTest.Status.values().length == 0) ? null : UUID.randomUUID().toString();
+         Sale sale = new Sale(id, code, status.name(), day);
+         bulk.put(sale.getId(), sale);
       }
+      return bulk;
    }
 }
